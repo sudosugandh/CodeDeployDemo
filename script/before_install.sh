61a202c28378ecf2853f503e4787e56393103bd3
sugandhkumar01 <ksugandh0@gmail.com>
2022-09-07 10:55:29 +0530

    codedeploy

---
+++ script/before_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+FILE=/home/ubuntu/app/index.js
+if test -f "$FILE"; then
+   echo "$FILE exists"
+   cd /home/ubuntu/
+   pm2 stop --silent index.js
+   killall -9 node
+else
+cd /home/ubuntu/
+sudo rm -rf app
+sudo mkdir app
